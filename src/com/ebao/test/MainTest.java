26d19b56beecc390820e9f4c93e20adc98ddcc6d
maikmai <maik.mai@ebaotech.com>
2020-03-12 15:47:22 +0800

    1 commit

---
+++ src/com/ebao/test/MainTest.java
@@ -0,0 +1,10 @@
+package com.ebao.test;
+
+/**
+ * created by maik by 3/12/2020
+ */
+public class MainTest {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
+}
